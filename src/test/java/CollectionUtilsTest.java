026788d31e2f855d781ad160addc4493669b27af
Seaborn Lee <seabornlee.cn@gmail.com>
2018-07-10 15:16:36 +0800

    Init

---
+++ src/test/java/CollectionUtilsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class CollectionUtilsTest {
+    @Test
+    public void should_pass() {
+        assertThat(1).isGreaterThan(0);
+    }
+}
